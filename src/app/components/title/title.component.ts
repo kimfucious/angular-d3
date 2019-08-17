301e7fa373e48ddb7fd63e4d61af63cf1e611ae3
Kimfucious <github@kimford.me>
2019-08-17 19:06:37 +0700

    make semi-responive ;)

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BreakpointsService } from "src/app/services/breakpoints.service";
 
 @Component({
-  selector: 'app-title',
-  templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  selector: "app-title",
+  templateUrl: "./title.component.html",
+  styleUrls: ["./title.component.css"]
 })
 export class TitleComponent implements OnInit {
-
-  constructor() { }
-
+  doughnutStyles = {};
+  operatorStyles = {};
+  titleImageStyles = {};
+  constructor(private breakpointsService: BreakpointsService) {}
   ngOnInit() {
+    this.breakpointsService.breakpoints.subscribe(res => {
+      const isSmall = res.matches;
+      this.titleImageStyles = {
+        height: isSmall ? "72px" : "128px",
+        width: isSmall ? "72px" : "128px"
+      };
+      this.doughnutStyles = {
+        "font-size": isSmall ? "72px" : "120px"
+      };
+      this.operatorStyles = {
+        "font-size": isSmall ? "28px" : "56px"
+      };
+    });
   }
-
 }
